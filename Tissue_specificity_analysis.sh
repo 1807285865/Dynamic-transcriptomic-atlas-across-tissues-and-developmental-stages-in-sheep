c426ee2235d8bbe520085e3ed580feb45be44a25
liming <115200673+1807285865@users.noreply.github.com>
2025-11-29 22:30:01 +0800

    Tissue_specificity_analysis

---
+++ Tissue_specificity_analysis.sh
@@ -0,0 +1,11 @@
+# 1. Calculate seven distinct matrices to measure the tissue specificity
+tspex --log tissue_median.csv tissue_median_tau.tsv tau
+tspex --log tissue_median.csv tissue_median_gini.tsv gini
+tspex --log tissue_median.csv tissue_median_simpson.tsv simpson
+tspex --log tissue_median.csv tissue_median_shannon_specificity.tsv shannon_specificity
+tspex --log tissue_median.csv tissue_median_roku_specificity.tsv roku_specificity
+tspex --log tissue_median.csv tissue_median_spm_dpm.tsv spm_dpm
+tspex --log tissue_median.csv tissue_median_js_specificity_dpm.tsv js_specificity_dpm
+
+# 2. Detect tissue-specific genes
+script: "Detect_tissue-specific_genes.R"
